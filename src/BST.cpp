61e0b4bdfb0df19ab4e5483f5980895f771d1356
GavinFrazar <gavinfrazar@gmail.com>
2018-02-06 00:27:47 -0800

    added BST definitions

--- src/BST.cpp
+++ src/BST.cpp
@@ -1,31 +1,129 @@
 #include "BST.h"
 
+void BST::delete_tree(Node *& root)
+{
+    using std::vector;
+    vector<Node*> targets;
+    targets.push_back(root);
+    for (auto &target : targets)
+    {
+        if (target->left != nullptr)
+            targets.push_back(target->left);
+        if (target->right != nullptr)
+            targets.push_back(target->right);
+        delete target;
+    }
+}
+
+Node * BST::search_tree(const std::string& word)
+{
+    Node* target = this->root;
+    while (target != nullptr)
+    {
+        if (target->word == word)
+            break;
+        else if (word < target->word)
+            target = target->left;
+        else if (word > target->word)
+            target = target->right;
+    }
+    return target;
+}
+
 BST::BST()
 {
+    this->root = nullptr;
 }
 
 BST::~BST()
 {
+    this->delete_tree(this->root);
 }
 
-bool BST::search_word(std::string word)
+bool BST::search_word(const std::string& word)
 {
-    return false;
+    Node* result = search_tree(word);
+    if (result != nullptr)
+        return true;
+    else
+        return false;
 }
 
-void BST::insert_word(std::string word)
+void BST::insert_word(const std::string &word)
 {
+    Node* target = this->root;
+    while (target != nullptr)
+    {
+        if (word == target->word)
+        {
+            ++target->counter;
+            return;
+        }
+        else if (word < target->word)
+            target = target->left;
+        else if (word > target->word)
+            target = target->right;
+    }
+    target = new Node(word);
 }
 
 void BST::delete_word(std::string word)
 {
+    Node* target = this->root;
+    while (target != nullptr)
+    {
+        if (target->word == word)
+        {
+            if (target->counter == 0)
+            {
+                //replace deleted node with left-most node
+                Node* replacement;
+                if (target->right != nullptr)
+                {
+                    replacement = target->right;
+                    while (replacement->left != nullptr)
+                    {
+                        replacement = replacement->left;
+                    }
+                    //TODO---delete and replace
+
+                }
+            }
+            else
+                --target->counter;
+        }
+    }
 }
 
 void BST::sort_tree()
 {
 }
 
-std::vector<std::string> BST::range(std::string word1, std::string word2)
+std::vector<std::string&> BST::range(std::string word1, std::string word2)
 {
-    return std::vector<std::string>();
+    Node* min = this->root;
+    std::vector<Node*> targets;
+    while (min != nullptr)
+    {
+        if (word1 == min->word)
+        {
+
+        }
+        else if (word1 < min->word)
+        {
+            targets.push_back(min);
+            min = min->left;
+        }
+    }
+    
+    std::vector<std::string&> words;
+    for (auto &target : targets)
+    {
+        if (target->left != nullptr)
+            targets.push_back(target->left);
+        if (target->right != nullptr)
+            targets.push_back(target->right);
+        words.push_back(target->word);
+    }
+    return words;
 }
--- src/BST.h
+++ src/BST.h
@@ -10,51 +10,66 @@ struct Node
     unsigned int counter;
     Node* left;
     Node* right;
+
+    Node()
+    { 
+        this->counter = 0;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+    Node(const std::string &word)
+    {
+        this->word = word;
+    }
 };
 
 class BST
 {
     private:
     protected:
+        Node* root;
+        //deletes a tree
+        void delete_tree(Node* &root);
+        Node* search_tree(const std::string& word);
     public:
-    //ctor
-    BST();
+        //ctor
+        BST();
 
-    //dtor
-    ~BST();
+        //dtor
+        ~BST();
 
-    /*
-    (b)
-    A function for searching a word in the tree (the word may or may not exist).
-    */
-    bool search_word(std::string word);
+        /*
+        (b)
+        A function for searching a word in the tree (the word may or may not exist).
+        */
+        bool search_word(const std::string& word);
 
-    /*
-    (c)
-    A function for inserting a new word into the tree or increment the counter if it
-    is already inserted.
-    */
-    void insert_word(std::string word);
+        /*
+        (c)
+        A function for inserting a new word into the tree or increment the counter if it
+        is already inserted.
+        */
+        void insert_word(const std::string &word);
 
-    /*
-    (d)
-    A function for deleting a word from the tree if the counter reaches ZERO
-    or decrementing the counter by one. Words that do not exist in tree should be ignored.
-    */
-    void delete_word(std::string word);
+        /*
+        (d)
+        A function for deleting a word from the tree if the counter reaches ZERO
+        or decrementing the counter by one. Words that do not exist in tree should be ignored.
+        */
+        void delete_word(std::string word);
 
-    /*
-    (e)
-    A function to sort the words in the tree lexicographically.
-    */
-    void sort_tree();
+        /*
+        (e)
+        A function to sort the words in the tree lexicographically.
+        */
+        void sort_tree();
 
-    /*
-    (f)
-    A function for doing a range search. The function takes as input two words. Given two
-    words, the function should find all the words in between. The resulting words need not
-    be sorted.
-    */
-    std::vector<std::string> range(std::string word1, std::string word2);
+        /*
+        (f)
+        A function for doing a range search. The function takes as input two words. Given two
+        words, the function should find all the words in between. The resulting words need not
+        be sorted.
+        */
+        std::vector<std::string&> range(std::string word1, std::string word2);
 };
 #endif
