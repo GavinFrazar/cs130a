cd6b95391f5891c36edce7c4bc0f25c3028cadbb
GavinFrazar <gavinfrazar@gmail.com>
2018-02-05 21:47:46 -0800

    getting set up

---
+++ src/BST.h
@@ -0,0 +1,60 @@
+#ifndef BST_H
+#define BST_H
+
+#include <string>
+#include <vector>
+
+struct Node
+{
+    std::string word;
+    unsigned int counter;
+    Node* left;
+    Node* right;
+};
+
+class BST
+{
+    private:
+    protected:
+    public:
+    //ctor
+    BST();
+
+    //dtor
+    ~BST();
+
+    /*
+    (b)
+    A function for searching a word in the tree (the word may or may not exist).
+    */
+    bool search_word(std::string word);
+
+    /*
+    (c)
+    A function for inserting a new word into the tree or increment the counter if it
+    is already inserted.
+    */
+    void insert_word(std::string word);
+
+    /*
+    (d)
+    A function for deleting a word from the tree if the counter reaches ZERO
+    or decrementing the counter by one. Words that do not exist in tree should be ignored.
+    */
+    void delete_word(std::string word);
+
+    /*
+    (e)
+    A function to sort the words in the tree lexicographically.
+    */
+    void sort_tree();
+
+    /*
+    (f)
+    A function for doing a range search. The function takes as input two words. Given two
+    words, the function should find all the words in between. The resulting words need not
+    be sorted.
+    */
+    std::vector<std::string> range(std::string word1, std::string word2);
+};
+#endif
